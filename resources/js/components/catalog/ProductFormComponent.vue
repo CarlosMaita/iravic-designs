8882026be193d9f4f6995cdd0852461cdc2a6dc4
Jonathan Cuotto <jonathancuotto@gmail.com>
2021-07-21 16:52:44 -0400

    Color bloques de form producto en tabs

--- resources/js/components/catalog/ProductFormComponent.vue
+++ resources/js/components/catalog/ProductFormComponent.vue
@@ -1,76 +1,86 @@
 <template>
     <div class="mb-3">
-        <!--  -->
-        <div>
-            <div class="row">
-                <div class="col-12">
-                    <div class="form-check form-check-inline mb-4">
-                        <input class="form-check-input" type="checkbox" name="is_regular" id="is_regular" value="1" 
-                        v-model="is_regular">
-                        <label class="form-check-label" for="is_regular">Es producto regular (Sin combinaciones)</label>
-                    </div>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-12">
-                    <div class="form-group">
-                        <label for="name">Nombre</label>
-                        <input class="form-control" id="name" name="name" type="text" v-model="product.name" autofocus>
-                    </div>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-6">
-                    <div class="form-group">
-                        <label for="code">Código</label>
-                        <input class="form-control" id="code" name="code" type="text" v-model="product.code">
-                    </div>
-                </div>
-                <div class="col-6">
-                    <div class="form-group">
-                        <label for="gender">Género</label>
-                        <select class="form-control" id="gender" name="gender" v-model="product.gender">
-                            <option :value="null" selected disabled>Seleccionar</option>
-                            <option v-for="(gender,index) in genders" :value="gender" :key="`gender-${index}`">{{ gender }}</option>
-                        </select>
-                    </div>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-6">
-                    <div class="form-group">
-                        <label for="category_id">Categoría</label>
-                        <select class="form-control" id="category" name="category_id" v-model="product.category_id">
-                            <option :value="null" selected disabled>Seleccionar</option>
-                            <option v-for="(category,index) in categories" :value="category.id" :key="`category-${index}`">{{ category.name }}</option>
-                        </select>
-                    </div>
-                </div>
-                <div class="col-6">
-                    <div class="form-group">
-                        <label for="brand_id">Marca</label>
-                        <select class="form-control" id="brand" name="brand_id" v-model="product.brand_id">
-                            <option :value="null" selected disabled>Seleccionar</option>
-                            <option v-for="(brand,index) in brands" :value="brand.id" :key="`brand-${index}`">{{ brand.name }}</option>
-                        </select>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <!--  -->
-        <br>
         <ul class="nav nav-tabs" id="myTab" role="tablist">
+            <li class="nav-item">
+                <a class="nav-link active" id="info-tab" data-toggle="tab" href="#info" role="tab" aria-controls="info" aria-selected="true">Info</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" id="multimedia-tab" data-toggle="tab" href="#multimedia" role="tab" aria-controls="multimedia" aria-selected="true">Multimedia</a>
+            </li>
             <li v-if="is_regular" class="nav-item">
-                <a :class="`nav-link ${is_regular ? 'active' : ''}`" id="stocks-tab" data-toggle="tab" href="#stocks" role="tab" aria-controls="stocks" aria-selected="true">Atributos y Stocks</a>
+                <a class="nav-link" id="stocks-tab" data-toggle="tab" href="#stocks" role="tab" aria-controls="stocks" aria-selected="false">Atributos y Stocks</a>
             </li>
             <li v-if="!is_regular" class="nav-item">
-                <a :class="`nav-link ${!is_regular ? 'active' : ''}`" id="combinations-tab" data-toggle="tab" href="#combinations" role="tab" aria-controls="combinations" aria-selected="false">Combinaciones y Stocks</a>
+                <a class="nav-link" id="combinations-tab" data-toggle="tab" href="#combinations" role="tab" aria-controls="combinations" aria-selected="false">Combinaciones y Stocks</a>
             </li>
         </ul>
         <!--  -->
         <div class="tab-content" id="myTabContent">
             <!--  -->
-            <div :class="`tab-pane fade ${is_regular ? 'show active' : ''}`" id="stocks" role="tabpanel" aria-labelledby="stocks-tab">
+            <div class="tab-pane fade show active" id="info" role="tabpanel" aria-labelledby="info-tab">
+                <!--  -->
+                <div class="row mt-3">
+                    <div class="col-12">
+                        <div class="form-check form-check-inline mb-4">
+                            <input class="form-check-input" type="checkbox" name="is_regular" id="is_regular" value="1" 
+                            v-model="is_regular">
+                            <label class="form-check-label" for="is_regular">Es producto regular (Sin combinaciones)</label>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-12">
+                        <div class="form-group">
+                            <label for="name">Nombre</label>
+                            <input class="form-control" id="name" name="name" type="text" v-model="product.name" autofocus>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-6">
+                        <div class="form-group">
+                            <label for="code">Código</label>
+                            <input class="form-control" id="code" name="code" type="text" v-model="product.code">
+                        </div>
+                    </div>
+                    <div class="col-6">
+                        <div class="form-group">
+                            <label for="gender">Género</label>
+                            <select class="form-control" id="gender" name="gender" v-model="product.gender">
+                                <option :value="null" selected disabled>Seleccionar</option>
+                                <option v-for="(gender,index) in genders" :value="gender" :key="`gender-${index}`">{{ gender }}</option>
+                            </select>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-6">
+                        <div class="form-group">
+                            <label for="category_id">Categoría</label>
+                            <select class="form-control" id="category" name="category_id" v-model="product.category_id">
+                                <option :value="null" selected disabled>Seleccionar</option>
+                                <option v-for="(category,index) in categories" :value="category.id" :key="`category-${index}`">{{ category.name }}</option>
+                            </select>
+                        </div>
+                    </div>
+                    <div class="col-6">
+                        <div class="form-group">
+                            <label for="brand_id">Marca</label>
+                            <select class="form-control" id="brand" name="brand_id" v-model="product.brand_id">
+                                <option :value="null" selected disabled>Seleccionar</option>
+                                <option v-for="(brand,index) in brands" :value="brand.id" :key="`brand-${index}`">{{ brand.name }}</option>
+                            </select>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!--  -->
+            <div class="tab-pane fade" id="multimedia" role="tabpanel" aria-labelledby="multimedia-tab">
+                <!--  -->
+                <p>Multimedia</p>
+            </div>
+            <!--  -->
+            <div class="tab-pane fade" id="stocks" role="tabpanel" aria-labelledby="stocks-tab">
                 <!--  -->
                 <div class="row mt-3">
                     <div class="col-md-6">
@@ -115,7 +125,7 @@
                 </div>
             </div>
             <!--  -->
-            <div :class="`tab-pane fade ${!is_regular ? 'show active' : ''}`" id="combinations" role="tabpanel" aria-labelledby="combinations-tab">
+            <div class="`tab-pane fade" id="combinations" role="tabpanel" aria-labelledby="combinations-tab">
                 <div class="d-flex justify-content-end my-3">
                     <button class="btn btn-light" type="button" @click="addCombination"><i class="fas fa-plus"></i> Agregar Combinación</button>
                 </div>
