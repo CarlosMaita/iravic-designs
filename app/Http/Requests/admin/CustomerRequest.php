5ce2b057e5c38c370f1e22762b68a03c50545fe6
Jonathan Cuotto <jonathancuotto@gmail.com>
2021-07-22 12:59:17 -0400

    Custom validaciones de clientes sobe dni existente y persona de contacto

--- app/Http/Requests/admin/CustomerRequest.php
+++ app/Http/Requests/admin/CustomerRequest.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Requests\admin;
 
+use App\Models\Customer;
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Validation\Rule;
 
@@ -11,15 +12,12 @@ class CustomerRequest extends FormRequest
     {
         return [
             'address.required' => 'El campo dirección es obligatorio',
-            // 'contact_name',
-            // 'contact_telephone',
-            // 'contact_dni',
             'dni.required' => 'El campo C.I es obligatorio.',
             'dni_picture.required' => 'La foto de la C.I es obligatoria.',
             'max_credit.required' => 'El campo Crédito Máximo es obligatorio.',
             'name.required' => 'El campo nombre es obligatorio.',
             'name.min' => 'El campo nombre debe tener por lo menos :min caracteres.',
-            // 'receipt_picture',
+            'receipt_picture.required' => 'La foto del recibo de sueldo es obligatoria.',
             'qualification.required' => 'El campo calificación del cliente es obligatorio.',
             'qualification.in' => 'El campo de calificación seleccionado no es válido.',
             'telephone.required' => 'El campo teléfono es obligatorio.',
@@ -47,9 +45,6 @@ class CustomerRequest extends FormRequest
     {
         return [
             'address' => 'required',
-            // 'contact_name' => '',
-            // 'contact_telephone' => '',
-            // 'contact_dni' => '',
             'dni' => 'required',
             // 'dni_picture.required' => '',
             'max_credit' => 'required|numeric',
@@ -72,6 +67,32 @@ class CustomerRequest extends FormRequest
         if ($this->address && (!$this->latitude || !$this->longitude)) {
             $validator->after(function ($validator) {
                 $validator->errors()->add('coords', 'No se ha encontrado latitud y longitud para la dirección.');
+            });
+        }
+
+        /**
+         * Custom Unique rule for DNI
+         */
+        if ($this->dni) {
+            if ($existing_customer = Customer::where('dni', $this->dni)->first()) {
+                $validator->after(function ($validator) use($existing_customer) {
+                    $validator->errors()->add('dni_used', 'El DNI ingresado se encuentra registrado para el cliente <b>' . $existing_customer->name . '</b>');
+                });
+            }
+
+            if ($existing_customer = Customer::where('contact_dni', $this->dni)->first()) {
+                $validator->after(function ($validator) use ($existing_customer) {
+                    $validator->errors()->add('dni_used', 'El DNI ingresado se encuentra registrado como DNI de contacto para el cliente <b>' . $existing_customer->name . '</b>');
+                });
+            }
+        }
+
+        /**
+         * There should be a contact name if contact telephone o contact dni is present
+         */
+        if (!$this->contact_name && ($this->contact_telephone || $this->contact_dni)) {
+            $validator->after(function ($validator) {
+                $validator->errors()->add('contact_info', 'Debe ingresar el nombre de la persona de contacto');
             });
         }
     }
