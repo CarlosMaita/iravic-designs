ab347652eab606b5ba02030e6c4267e9a250eeb9
Jonathan Cuotto <jonathancuotto@gmail.com>
2021-08-20 23:41:46 -0400

    Fix title de template html

--- resources/views/auth/base.blade.php
+++ resources/views/auth/base.blade.php
@@ -13,7 +13,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
-    <meta name="description" content="">
+    <meta name="description" content="Panel administrativo para venta de Zapatos y Ropa">
     <meta name="author" content="Brocsoft">
     <meta name="keyword" content="">
     <title>{{ config('app.name') }}</title>
