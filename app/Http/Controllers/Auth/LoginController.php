394af31761b36c34bf998347f8b320d19a1c61b6
Carlos Maita <carlosmaita2009@gmail.com>
2023-10-27 12:50:43 -0400

    bug - ajustado para que usuarios sin rol no tengan acceso al dashboard

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Models\Role;
+use App\User;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -50,11 +52,26 @@ class LoginController extends Controller
     {
         $credentials = $request->only('email', 'password');
         $remember = ($request->has('remember_me')) ? true : false;
-
-        if (Auth::attempt($credentials, $remember)) {
-            return redirect($this->redirectTo);
+        #validar existencia de usuario
+        $user = User::where('email',$credentials['email'])->first();
+        // Verifica si el usuario existe
+        if ($user) {
+            // Verifica si existe un rol asignado
+            $role_user = $user->roles;
+            if (count($role_user) > 0 ){
+                if (Auth::attempt($credentials, $remember)) {
+                    return redirect($this->redirectTo);
+                }
+                return back()->with(['message' => 'Credenciales inválidas.']);
+            }else{
+                // El usuario no tiene rol vinvulado, muestra un mensaje de error
+                return redirect()->back()->with(['message' => 'El rol no existe o no cuenta con acceso.']);
+            }
+    
+        }else{
+             // El usuario no existe, muestra un mensaje de error
+            return redirect()->back()->with(['message' => 'El usuario no existe.']);
         }
 
-        return back()->with(['message' => 'Credenciales inválidas.']);
     }
 }
