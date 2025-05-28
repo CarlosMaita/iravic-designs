b457c669eca60d83306c1155c5132b1615f98e14
Carlos Maita <cmaita@brocsoft.com>
2025-05-28 17:29:14 -0400

    Ajuste de aparicion de boton de filtro en mobile

--- resources/views/store/catalog/index.blade.php
+++ resources/views/store/catalog/index.blade.php
@@ -22,3 +22,12 @@
     :category="{{ json_encode($category) }}"
     ></catalog-ecommerce-component>
 @endsection
+
+
+@section('bottom-filter-buttom')
+  <!-- Filter offcanvas toggle that is visible on screens < 992px wide (lg breakpoint) -->
+    <button type="button" class="fixed-bottom z-sticky w-100 btn btn-lg btn-dark border-0 border-top border-light border-opacity-10 rounded-0 pb-4 d-lg-none" data-bs-toggle="offcanvas" data-bs-target="#filterSidebar" aria-controls="filterSidebar" data-bs-theme="light">
+      <i class="ci-filter fs-base me-2"></i>
+        Filtros
+    </button>
+@endsection
