df8adbef18a322c0c5b87b7ed3ade0d8cb09957d
Carlos Maita <cmaita@brocsoft.com>
2025-06-17 23:08:59 -0400

    Ajsutes en el header

--- resources/views/store/shared/header.blade.php
+++ resources/views/store/shared/header.blade.php
@@ -1,28 +1,82 @@
  <header class="navbar navbar-expand-lg navbar-sticky bg-body d-lg-block sticky-top p-0" data-sticky-navbar='{"offset": 100}'>
       <div class="container py-2 py-lg-3">
-        <div class="d-flex align-items-center gap-3">
-          <!-- Mobile offcanvas menu toggler (Hamburger) -->
-          <button type="button" class="navbar-toggler me-4 me-md-2" data-bs-toggle="offcanvas" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>
-        </div>
 
-        <!-- Navbar brand (Logo) -->
-        <a class="navbar-brand d-flex align-items-center justify-content-end fs-2 py-0 m-0 me-auto me-sm-n5" href="{{route('ecommerce.home')}}">
-          <h6 class="m-0">Iravic Designs</h6>
-        </a>
+          <!-- Categories mega menu -->
+          <div class=" navbar-nav d-none d-lg-flex align-items-center justify-content-between flex-wrap gap-3 me-lg-3 mb-2 mb-lg-0">
+            
+            <div class="dropdown position-relative pb-lg-2">
+              <button type="button" class="nav-link animate-underline fw-semibold text-uppercase ps-0" data-bs-toggle="dropdown" data-bs-trigger="hover" data-bs-auto-close="outside" aria-expanded="false">
+                <i class="ci-menu fs-lg me-2"></i>
+                <span class="animate-target">Categorias</span>
+              </button>
+              <div class="dropdown-menu p-4 px-xl-5" style="--cz-dropdown-spacer: .5rem; width: max-content;">
+
+                <div class="row g-4">
+                    <div class="col-12">
+                      <h6 class="mb-3">Todas las categorias</h6>
+                    </div>
+                    @php
+                      $chunks = $categories->chunk(ceil($categories->count() / 4));
+                      @endphp
+                    @foreach ($chunks as $chunk)
+                    <div class="col-lg-3">
+                      <ul class="nav flex-column gap-2 mt-0">
+                        @foreach ($chunk as $category)
+                          <li class="d-flex w-100 pt-1">
+                            <a class="nav-link animate-underline animate-target d-inline fw-normal p-0" href="{{ route('ecommerce.home') . '?category=' . $category->id }}">{{ $category->name }}</a>
+                          </li>
+                        @endforeach
+                      </ul>
+                    </div>
+                    @endforeach
+                </div>
+
+              </div>
+            </div>
+          </div>
+
+          
+          <div class="d-flex d-lg-none align-items-center gap-3">
+            <!-- Mobile offcanvas menu toggler (Hamburger) -->
+            <button type="button" class="navbar-toggler me-4 me-md-2" data-bs-toggle="offcanvas" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+          </div>
+
+          <!-- Navbar brand (Logo) -->
+          <a class="navbar-brand d-flex align-items-center justify-content-end fs-2 py-0 m-0 me-auto me-sm-n5" href="{{route('ecommerce.home')}}">
+                  <h6 class="m-0 text-uppercase" style="font-family: Roboto, sans-serif; letter-spacing: 3px;">Iravic Designs</h6>
+          </a>
+
+
+
+
 
         <!-- Button group -->
         <div class="d-flex align-items-center">
+
+
+
+            <!-- Search toggle button visible on screens < 992px wide (lg breakpoint) -->
+          <button  type="button" class="btn btn-icon btn-lg fs-xl btn-outline-secondary border-0 rounded-circle animate-shake" data-bs-toggle="offcanvas" data-bs-target="#searchBox" aria-controls="searchBox" aria-label="Toggle search bar" >
+            <i class="ci-search animate-target"></i>
+          </button>
 
           <!-- Navbar stuck nav toggler -->
           <button type="button" class="navbar-toggler d-none navbar-stuck-show me-3" data-bs-toggle="collapse" data-bs-target="#stuckNav" aria-controls="stuckNav" aria-expanded="false" aria-label="Toggle navigation in navbar stuck state">
             <span class="navbar-toggler-icon"></span>
           </button>
 
+          
+          <!-- Account button visible on screens > 768px wide (md breakpoint) -->
+          <a class="btn btn-icon btn-sm fs-lg btn-outline-secondary border-0 rounded-circle animate-shake" href="{{route('customer.login.form')}}">
+            <i class="ci-user animate-target"></i>
+            <span class="visually-hidden">Cuenta</span>
+          </a>
+
           <!-- Theme switcher (light/dark/auto) -->
           <div class="dropdown">
-            <button type="button" class="theme-switcher btn btn-icon btn-lg btn-outline-secondary fs-lg border-0 rounded-circle animate-scale" data-bs-toggle="dropdown" aria-expanded="false" aria-label="Toggle theme (light)">
+            <button type="button" class="theme-switcher btn btn-icon d-none  d-md-flex btn-lg btn-outline-secondary fs-lg border-0 rounded-circle animate-scale" data-bs-toggle="dropdown" aria-expanded="false" aria-label="Toggle theme (light)">
               <span class="theme-icon-active d-flex animate-target">
                 <i class="ci-sun"></i>
               </span>
@@ -57,32 +111,16 @@
               </li>
             </ul>
           </div>
-
-          <!-- Search toggle button visible on screens < 992px wide (lg breakpoint) -->
-          <button  type="button" class="btn btn-icon btn-lg fs-xl btn-outline-secondary border-0 rounded-circle animate-shake d-lg-none" data-bs-toggle="offcanvas" data-bs-target="#searchBox" aria-controls="searchBox" aria-label="Toggle search bar" >
-            <i class="ci-search animate-target"></i>
-          </button>
-
-          <!-- Account button visible on screens > 768px wide (md breakpoint) -->
-          {{-- <a class="btn btn-icon btn-lg fs-lg btn-outline-secondary border-0 rounded-circle animate-shake d-none d-md-inline-flex" href="{{route('customer.login.form')}}">
-            <i class="ci-user animate-target"></i>
-            <span class="visually-hidden">Cuenta</span>
-          </a> --}}
-
+          
           <!-- Wishlist button visible on screens > 768px wide (md breakpoint) -->
           {{-- <a class="btn btn-icon btn-lg fs-lg btn-outline-secondary border-0 rounded-circle animate-pulse d-none d-md-inline-flex" href="#!">
             <i class="ci-heart animate-target"></i>
             <span class="visually-hidden">Deseos</span>
           </a> --}}
-
+          
           <!-- Cart button -->
           <icon-header-cart-ecommerce-component ref="iconHeaderCartEcommerceComponent"></icon-header-cart-ecommerce-component>
-          
-          {{--           
-          <button type="button" class="btn btn-icon btn-lg fs-xl btn-outline-secondary position-relative border-0 rounded-circle animate-scale" data-bs-toggle="offcanvas" data-bs-target="#shoppingCart" aria-controls="shoppingCart" aria-label="Shopping cart">
-            <span v-if="cart.items.length > 0" class="position-absolute top-0 start-100 badge fs-xs text-bg-primary rounded-pill mt-1 ms-n4 z-2" style="--cz-badge-padding-y: .25em; --cz-badge-padding-x: .42em">{{ cart.items.length }}</span>
-            <i class="ci-shopping-bag animate-target me-1"></i>
-          </button> --}}
+
         </div>
       </div>
 
@@ -103,10 +141,6 @@
                 <div class="navbar-nav d-none d-lg-flex align-items-center justify-content-between flex-wrap gap-3 me-lg-3 mb-2 mb-lg-0">
                   
                   <div class="dropdown position-static pb-lg-2">
-                    <button type="button" class="nav-link animate-underline fw-semibold text-uppercase ps-0" data-bs-toggle="dropdown" data-bs-trigger="hover" data-bs-auto-close="outside" aria-expanded="false">
-                      <i class="ci-menu fs-lg me-2"></i>
-                      <span class="animate-target">Categorias</span>
-                    </button>
                     <div class="dropdown-menu w-100 p-4 px-xl-5" style="--cz-dropdown-spacer: .75rem">
 
                       <div class="row g-4">
@@ -134,28 +168,29 @@
                 </div>
 
                 <!-- Search form visible on screens < 992px wide (lg breakpoint) -->
-                  <div class="row d-lg-none">
-                      <div class="col-12">
-                        <h6 class="mb-3">Todas las categorias</h6>
-                      </div>
-                      <div class="col-lg-3">
-                        <ul class="nav flex-column gap-2 mt-0">
-                          @foreach ($categories as $category)
-                          <li class="d-flex w-100 pt-1">
-                              <a class="nav-link animate-underline animate-target d-inline fw-normal text-truncate p-0" href="{{ route('ecommerce.home') . '?category=' . $category->id }}">{{ $category->name }}</a>
-                            </li>
-                            @endforeach
-                        </ul>
-                      </div>
-                  </div>
+                <div class="row d-lg-none">
+                    <div class="col-12">
+                      <h6 class="mb-3">Todas las categorias</h6>
+                    </div>
+                    <div class="col-lg-3">
+                      <ul class="nav flex-column gap-2 mt-0">
+                        @foreach ($categories as $category)
+                        <li class="d-flex w-100 pt-1">
+                            <a class="nav-link animate-underline animate-target d-inline fw-normal text-truncate p-0" href="{{ route('ecommerce.home') . '?category=' . $category->id }}">{{ $category->name }}</a>
+                          </li>
+                          @endforeach
+                      </ul>
+                    </div>
+                </div>
                       
 
                        
                 <!-- Search toggle visible on screens > 991px wide (lg breakpoint) -->
-                <button type="button" class="btn btn-outline-secondary justify-content-start w-100 px-3 mb-lg-2 ms-3 d-none d-lg-inline-flex" style="max-width: 240px" data-bs-toggle="offcanvas" data-bs-target="#searchBox" aria-controls="searchBox">
+                {{-- <button type="button" class="btn btn-outline-secondary justify-content-start w-100 px-3 mb-lg-2 ms-3 d-none d-lg-inline-flex" style="max-width: 240px" data-bs-toggle="offcanvas" data-bs-target="#searchBox" aria-controls="searchBox">
                   <i class="ci-search fs-base ms-n1 me-2"></i>
                   <span class="text-body-tertiary fw-normal">Buscar</span>
-                </button>
+                </button> --}}
+                
               </div>
             </div>
           </div>
@@ -176,4 +211,3 @@
         </nav>
       </div>
     </header>
-      
