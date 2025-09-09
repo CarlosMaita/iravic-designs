05276428bb4934627f146987bbb8bb84462d97d8
Carlos Maita <cmaita@brocsoft.com>
2025-09-08 23:02:46 -0400

    Ajuste

--- resources/views/ecommerce/home/index.blade.php
+++ resources/views/ecommerce/home/index.blade.php
@@ -22,15 +22,13 @@
 
   {{--  Carousel --}}
   @include('ecommerce.home.partials.carousel', ['banners' => $banners])
-
+  
   {{--  Special Offers --}}
   @include('ecommerce.home.partials.special-offers', ['specialOffers' => $specialOffers])
   
-  {{-- Main Categories  --}}
-  {{-- @include('ecommerce.home.partials.main-categories') --}}
-
   {{--  Featured Products --}}
   @include('ecommerce.home.partials.featured-products', ['featuredProducts' => $featuredProducts])
+  
 
 @endsection
 
